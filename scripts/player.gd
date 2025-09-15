ad0cfc34e3f21b3fe30bef405d64b008e3dcc987
vbowden3353-creator <vbowden3353@student.pps.net>
2025-09-15 09:52:04 -0700

    player animations
    created idle and walking animations for the payer

--- scripts/player.gd
+++ scripts/player.gd
@@ -31,10 +31,10 @@ func _physics_process(_delta):
 	# This gives us the actual pixels to move this frame
 	# If direction is 1 and speed is 300, we get 300 pixels right
 	# If direction is -1 and speed is 300, we get -300 pixels (left)
-	
+	velocity.x = xSpeed * xDirection
 	
 	# TODO: Calculate Y movement the same way
-	
+	velocity.y = ySpeed * yDirection
 	
 	# TODO: Set the player's velocity (how fast they're moving)
 	# Godot's CharacterBody2D uses a velocity system
@@ -45,21 +45,32 @@ func _physics_process(_delta):
 	# Use if statements to check xDirection and yDirection
 	# Set facing to "right", "left", "down", or "up"
 	# Only update facing when actually moving (direction != 0)
-	
+	if xDirection >0:
+		facing = "right"
+	elif xDirection <0:
+		facing = "left"
+	elif yDirection >0:
+		facing = "down"
+	elif yDirection <0:
+		facing = "up"
+
 	
 	# TODO: Update animation based on facing direction
 	# Call your update_animation() function here
 	
 	# TODO: Actually apply the movement
 	# This is a special Godot function that makes the movement happen
+	update_animation()
 	move_and_slide()
 
 # TODO: Create animation function (add this outside of _physics_process)
 func update_animation():
 	# TODO: Set the animation based on the facing direction
-	# Use: _animation_player.play("idle_" + facing)
+	if xDirection == 0 && yDirection == 0:
+		_animation_player.play("idle_" + facing)
+	elif xDirection !=0 || yDirection !=0:
+		_animation_player.play("walk_" + facing)
 	# This combines "idle_" with whatever direction we're facing
-	pass
 
 
 # TODO: Create health change function for interactions
