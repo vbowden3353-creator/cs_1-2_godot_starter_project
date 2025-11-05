c05dca636c5ce9f9d46c68d152a12ccf69090123
vbowden3353-creator <vbowden3353@student.pps.net>
2025-11-05 09:58:31 -0800

    Minotaur arrows and movement
    Work in progress

--- scenes/melee_minotaur.gd
+++ scenes/melee_minotaur.gd
@@ -1,11 +1,13 @@
 extends CharacterBody2D
+var projectile_original = preload("res://scenes/enemy_arrow.tscn")
 
 var in_range = false
 var chasing = false
 var attacking = false
-var speed = 200
+var speed = 200.0
 var health = 3
 var facing
+var direction = 0
 @onready var player: CharacterBody2D = %Player
 @onready var minotaur_animation: AnimatedSprite2D = $"minotaur animation"
 
@@ -16,20 +18,22 @@ func _ready():
 func _process(delta):
 	if in_range:
 		pass
+	
 	elif chasing and !attacking:
 		velocity = Vector2.ZERO
-		if player:
-			velocity = position.direction_to(player.position) * speed
-			move_and_slide()
-	elif chasing and attacking:
-		pass
-	elif !in_range and !chasing and !attacking:
-		pass
+		direction = position.direction_to(player.position)
 		
+	elif !chasing and !attacking:
+		pass
+	
+	if chasing == true:
+		position += direction * speed * delta
+
 func update_animation():
 	pass
 	if attacking:
 		minotaur_animation.play("attack_right")
+		print("minotaur attacking")
 	else:
 	# TODO: Set the animation based on the facing direction
 		if velocity.is_zero_approx():
@@ -43,10 +47,11 @@ func update_animation():
 func _on_melee_body_entered(body):
 	if body.name == "Player":
 		attacking = true
+		chasing = false
 		
 func _on_melee_body_exited(body):
 	if body.name == "Player":
-		pass
+		attacking = false
 		
 func _on_chase_body_entered(body):
 	if body.name == "Player":
@@ -61,7 +66,22 @@ func _on_chase_body_exited(body):
 func _on_ranged_body_entered(body):
 	if body.name == "Player":
 		in_range = true
+	if in_range:
+		shoot(player)
 	
 func _on_ranged_body_exited(body):
 	if body.name == "Player":
 		in_range = false
+
+func shoot(player):
+	# TODO: Create a new projectile instance
+	var projectile_clone = projectile_original.instantiate()
+	
+	# TODO: Set projectile position to player position
+	projectile_clone.global_position = position
+	
+	# TODO: Set projectile direction using facing variable
+	projectile_clone.set_direction(player.position)
+	
+	# TODO: Add projectile to the game world
+	get_tree().get_root().add_child(projectile_clone)
--- scripts/enemy_projectile.gd
+++ scripts/enemy_projectile.gd
@@ -1,4 +1,5 @@
 extends Area2D
+var projectile_original = preload("res://scenes/enemy_arrow.tscn")
 
 var direction
 var Speed = 300
@@ -19,4 +20,20 @@ func _physics_process(_delta):
 	
 func set_direction(target):
 	direction = position.direction_to(target)
+	if direction.x <0: print("togge to the left")
+	scale.x = -1
 	
+func shoot(body):
+	# TODO: Create a new projectile instance
+	var projectile_clone = projectile_original.instantiate()
+	
+	# TODO: Set projectile position to player position
+	projectile_clone.global_position = position
+	
+	# TODO: Set projectile direction using facing variable
+	projectile_clone.set_direction(body.position)
+	
+	# TODO: Add projectile to the game world
+	get_tree().get_root().add_child(projectile_clone)
+
+	pass
